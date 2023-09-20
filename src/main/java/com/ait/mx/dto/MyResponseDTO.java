0b24e54fa2f587399ed0e91719069f810d004d45
Diego Santillan <diego.santillan@soyait.mx>
2023-09-19 18:29:41 -0600

    Lombook

--- src/main/java/com/ait/mx/dto/MyResponseDTO.java
+++ src/main/java/com/ait/mx/dto/MyResponseDTO.java
@@ -1,30 +1,16 @@
 package com.ait.mx.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class MyResponseDTO {
 	
 	private String mensaje;
 	private String estado;
-	
-	public String getMensaje() {
-		return mensaje;
-	}
-	public void setMensaje(String mensaje) {
-		this.mensaje = mensaje;
-	}
-	public String getEstado() {
-		return estado;
-	}
-	public void setEstado(String estado) {
-		this.estado = estado;
-	}
-	public MyResponseDTO(String mensaje, String estado) {
-		this.mensaje = mensaje;
-		this.estado = estado;
-	}
-	
-	public MyResponseDTO() {
-	}
-	
-	
-
 }
